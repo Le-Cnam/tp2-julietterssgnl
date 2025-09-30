f03ed30c13ab113ca8531d4981a9c0deaea006cf
julietterssgnl <juliette.rossignol18@gmail.com>
2025-09-30 12:11:37 +0200

    Exercice 15

--- src/net/lecnam/ussi47/tp2/Exercice15.java
+++ src/net/lecnam/ussi47/tp2/Exercice15.java
@@ -1,9 +1,12 @@
 package net.lecnam.ussi47.tp2;
 
 public class Exercice15 {
-
     public static void main(String[] args) {
-
-
+        int hauteur = 10;
+        for (int i=0;i<hauteur;i++) {
+            for (int j=0;j<hauteur-i-1;j++) System.out.print(" ");
+            for (int j=0;j<2*i+1;j++) System.out.print("*");
+            System.out.println();
+        }
     }
 }
