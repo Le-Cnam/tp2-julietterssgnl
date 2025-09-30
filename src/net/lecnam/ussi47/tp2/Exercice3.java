2705745872dcda17dd1ed28ccb7cd6555852dac6
julietterssgnl <juliette.rossignol18@gmail.com>
2025-09-30 11:07:15 +0200

    Exercice 3

--- src/net/lecnam/ussi47/tp2/Exercice3.java
+++ src/net/lecnam/ussi47/tp2/Exercice3.java
@@ -4,6 +4,20 @@ public class Exercice3 {
 
     public static void main(String[] args) {
 
+        // Dimensions d'un carré de jardin
+        double longueur = 1.25 ;
+        double largeur = 1.25 ;
+        int nombreCarres = 4 ;
+        int plantesParCarre = 9 ;
 
+        // Calculs
+        double surfaceTotale = longueur * largeur * nombreCarres ;
+        int nombrePlantesTotales = plantesParCarre * nombreCarres ;
+        double surfaceParPlante = surfaceTotale / nombrePlantesTotales ;
+
+        // Affichage
+        System.out.println("Surface cultivée totale : " + surfaceTotale + " m²") ;
+        System.out.println("Nombre total de plantes : " + nombrePlantesTotales) ;
+        System.out.println("Surface par plante : " + surfaceParPlante + " m²") ;
     }
 }
