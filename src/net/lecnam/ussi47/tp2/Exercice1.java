bb98ea3cd97aa47c9fdb3babde98830a4b8f49bc
julietterssgnl <juliette.rossignol18@gmail.com>
2025-09-30 10:32:44 +0200

    Exercice 1

--- src/net/lecnam/ussi47/tp2/Exercice1.java
+++ src/net/lecnam/ussi47/tp2/Exercice1.java
@@ -3,7 +3,14 @@ package net.lecnam.ussi47.tp2;
 public class Exercice1 {
 
     public static void main(String[] args) {
-
-
+        byte premiereValeur = 12 ;
+        float deuxiemeValeur = 2.26f ;
+        short troisiemeValeur = -300 ;
+        long quatriemeValeur = 3000000000L ;
+        int cinquiemeValeur = 2000000000 ;
+        char sixiemeValeur = 'a' ;
+        String septiemeValeur = "Hello" ;
+        byte huitiemeValeur = 1 ;
+        int neuviemeValeur = 29000 ;
     }
 }
