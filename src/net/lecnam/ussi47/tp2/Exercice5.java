99553cb929a027827f542ec9ee34dd4456c8980e
julietterssgnl <juliette.rossignol18@gmail.com>
2025-09-30 11:26:48 +0200

    Exercice 5

--- src/net/lecnam/ussi47/tp2/Exercice5.java
+++ src/net/lecnam/ussi47/tp2/Exercice5.java
@@ -3,7 +3,18 @@ package net.lecnam.ussi47.tp2;
 public class Exercice5 {
 
     public static void main(String[] args) {
+        int puissanceRadiateur = 500;       // W
+        int volumeParRadiateur = 8;         // m³
 
+        float longueur = 3.40f;
+        float largeur = 4.30f;
+        float hauteur = 2.30f;
+        float volumePiece = longueur * largeur * hauteur;
 
+        // nombre de radiateurs nécesaires (arrondi à l'entier supérieur)
+        float nombreRadiateurs = volumePiece / volumeParRadiateur;
+
+        System.out.println("Volume de la pièce : " + volumePiece + " m³");
+        System.out.println("Nombre de radiateurs nécessaires : " + nombreRadiateurs);
     }
 }
