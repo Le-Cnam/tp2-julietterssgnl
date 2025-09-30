f9021df0b934e0a120fd6a2e90418aeebeb08d04
julietterssgnl <juliette.rossignol18@gmail.com>
2025-09-30 11:59:55 +0200

    Exercice 12

--- src/net/lecnam/ussi47/tp2/Exercice12.java
+++ src/net/lecnam/ussi47/tp2/Exercice12.java
@@ -1,9 +1,12 @@
 package net.lecnam.ussi47.tp2;
 
 public class Exercice12 {
-
     public static void main(String[] args) {
-
-
+        String phrase = "cette phrase contient 11 voyelles".toLowerCase();
+        int nombreVoyelles = 0;
+        for (char c : phrase.toCharArray()) {
+            if ("aeiouy".indexOf(c) >= 0) nombreVoyelles++;
+        }
+        System.out.println("Nombre de voyelles : " + nombreVoyelles);
     }
 }
