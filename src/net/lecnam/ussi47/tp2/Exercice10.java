29fb46acba753a77b938872b47a2863fd51c79c6
julietterssgnl <juliette.rossignol18@gmail.com>
2025-09-30 11:49:14 +0200

    Exercice 10

--- src/net/lecnam/ussi47/tp2/Exercice10.java
+++ src/net/lecnam/ussi47/tp2/Exercice10.java
@@ -1,9 +1,8 @@
 package net.lecnam.ussi47.tp2;
 
 public class Exercice10 {
-
     public static void main(String[] args) {
-
-
+        System.out.println("HELLO WORLD".toLowerCase());
+        System.out.println("hello world".toUpperCase());
     }
 }
