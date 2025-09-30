e337a29a4bd11bfdb134ea06f0bee28f6dbfeda9
julietterssgnl <juliette.rossignol18@gmail.com>
2025-09-30 11:47:48 +0200

    Exercice 9

--- src/net/lecnam/ussi47/tp2/Exercice9.java
+++ src/net/lecnam/ussi47/tp2/Exercice9.java
@@ -1,9 +1,8 @@
 package net.lecnam.ussi47.tp2;
 
 public class Exercice9 {
-
     public static void main(String[] args) {
-
-
+        double resultat = 3896.86 / 4.869;
+        System.out.println("3896,86 / 4,869 = " + resultat);
     }
 }
